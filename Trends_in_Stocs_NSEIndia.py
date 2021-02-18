26ad45d80f9b530b844a639964ac8044677d30ed
Anushka118 <39857792+Anushka118@users.noreply.github.com>
2021-02-18 11:43:47 +0530

    Update Trends_in_Stocs_NSEIndia.py

--- Trends_in_Stocs_NSEIndia.py
+++ Trends_in_Stocs_NSEIndia.py
@@ -4,7 +4,9 @@ import streamlit as st
 import pandas as pd
 
 #https://github.com/swapniljariwala/nsepy
-
+st.write("""
+# Trends in Stocks NSEIndia
+""")
 list_of_stocks = [
     'SBIN',
     'TATASTEEL',
@@ -15,9 +17,7 @@ list_of_stocks = [
     'RELIANCE'
     ]
 nseSymbol = st.radio('Select a stock', list_of_stocks)
-st.write("""
-# Stock Price App
-""")
+
 st.subheader(f'Stock Price of {nseSymbol} for the last 10 years')
 startDate = st.date_input('Enter Starting Date(YYYY-MM-DD):')
 endDate = st.date_input('Enter End Date(YYYY-MM-DD):')
